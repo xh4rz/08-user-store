d8ab276923681c3cdaba0f581f3aab8747907a8d
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-01-19 03:16:58 -0500

    266. AuthService

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,1 +1,2 @@
 export * from './errors/custom.error';
+export * from './dtos/auth/register-user.dto';
--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,11 +1,16 @@
 import { Request, Response } from 'express';
+import { RegisterUserDto } from '../../domain';
+import { AuthService } from '../services/auth.service';
 
 export class AuthController {
-	// DI
-	constructor() {}
+	constructor(public readonly authService: AuthService) {}
 
 	registerUser = (req: Request, res: Response) => {
-		res.json('registerUser');
+		const [error, registerDto] = RegisterUserDto.create(req.body);
+
+		if (error) return res.status(400).json({ error });
+
+		this.authService.registerUser(registerDto!).then((user) => res.json(user));
 	};
 
 	loginUser = (req: Request, res: Response) => {
