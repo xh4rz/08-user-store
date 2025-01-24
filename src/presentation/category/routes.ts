1b9ae6f2ae8de60d17256d7608fb7844b8d400c8
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-01-24 01:07:45 -0500

    295. Probar AuthMiddleware

--- src/presentation/category/routes.ts
+++ src/presentation/category/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { CategoryController } from './controller';
+import { AuthMiddleware } from '../middlewares/auth.middleware';
 
 export class CategoryRoutes {
 	static get routes(): Router {
@@ -9,7 +10,7 @@ export class CategoryRoutes {
 
 		router.get('/', controller.getCategories);
 
-		router.post('/', controller.createCategory);
+		router.post('/', [AuthMiddleware.validateJWT], controller.createCategory);
 
 		return router;
 	}
