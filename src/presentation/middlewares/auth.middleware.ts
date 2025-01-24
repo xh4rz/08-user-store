0cc2317810ae1a84df58fec25218d51e4d5e6c74
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-01-23 21:15:38 -0500

    293. AuthMiddleware - Proteger Rutas

---
+++ src/presentation/middlewares/auth.middleware.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from 'express';
+
+export class AuthMiddleware {
+	static async validateJWT(req: Request, res: Response, next: NextFunction) {
+		const authorization = req.header('Authorization');
+
+		if (!authorization)
+			return res.status(401).json({ error: 'No token provided' });
+
+		if (!authorization.startsWith('Bearer '))
+			return res.status(401).json({ error: 'Invalid Bearer token' });
+
+		const token = authorization.split(' ').at(1) || '';
+
+		try {
+		} catch (error) {
+			console.log(error);
+			res.status(500).json({ error: 'Internal server error' });
+		}
+	}
+}
