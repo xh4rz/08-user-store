5643c541f13daa6c722b13ffd17f56fe0e3a76e9
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-01-24 01:02:07 -0500

    294. AuthMiddleware - Validar token

--- src/presentation/middlewares/auth.middleware.ts
+++ src/presentation/middlewares/auth.middleware.ts
@@ -1,4 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
+import { JwtAdapter } from '../../config';
+import { UserModel } from '../../data';
+import { UserEntity } from '../../domain';
 
 export class AuthMiddleware {
 	static async validateJWT(req: Request, res: Response, next: NextFunction) {
@@ -13,6 +16,17 @@ export class AuthMiddleware {
 		const token = authorization.split(' ').at(1) || '';
 
 		try {
+			const payload = await JwtAdapter.validateToken<{ id: string }>(token);
+
+			if (!payload) return res.status(401).json({ error: 'Invalid token' });
+
+			const user = await UserModel.findById(payload.id);
+
+			if (!user) return res.status(401).json({ error: 'Invalid token - user' });
+
+			req.body.user = UserEntity.fromObject(user);
+
+			next();
 		} catch (error) {
 			console.log(error);
 			res.status(500).json({ error: 'Internal server error' });
