3fed133b57235b3c939bde5d577dbcf890b5fcdc
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-01-24 04:05:55 -0500

    298. Pagination DTO

--- src/presentation/category/controller.ts
+++ src/presentation/category/controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { CreateCategoryDto, CustomError } from '../../domain';
+import { CreateCategoryDto, CustomError, PaginationDto } from '../../domain';
 import { CategoryService } from '../services/category.service';
 
 export class CategoryController {
@@ -26,9 +26,17 @@ export class CategoryController {
 	};
 
 	getCategories = async (req: Request, res: Response) => {
-		this.categoryService
-			.getCategories()
-			.then((categories) => res.json(categories))
-			.catch((error) => this.handleError(error, res));
+		const { page = 1, limit = 10 } = req.query;
+
+		const [error, paginationDto] = PaginationDto.create(+page, +limit);
+
+		if (error) return res.status(400).json({ error });
+
+		res.json(paginationDto);
+
+		// this.categoryService
+		// 	.getCategories()
+		// 	.then((categories) => res.json(categories))
+		// 	.catch((error) => this.handleError(error, res));
 	};
 }
