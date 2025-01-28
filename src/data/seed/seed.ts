4df6a22d941a74c5822228a44ec5dfd5552ab051
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-01-28 04:28:37 -0500

    311. Llenar base de datos

--- src/data/seed/seed.ts
+++ src/data/seed/seed.ts
@@ -1,5 +1,11 @@
 import { envs } from '../../config';
-import { MongoDatabase } from '../mongo/mongo-database';
+import {
+	CategoryModel,
+	MongoDatabase,
+	ProductModel,
+	UserModel
+} from '../mongo';
+import { seedData } from './data';
 
 (async () => {
 	MongoDatabase.connect({
@@ -12,8 +18,42 @@ import { MongoDatabase } from '../mongo/mongo-database';
 	await MongoDatabase.disconnect();
 })();
 
+const randomBetween0AndX = (x: number) => {
+	return Math.floor(Math.random() * x);
+};
+
 async function main() {
+	// 0. borrar todo
+	await Promise.all([
+		UserModel.deleteMany(),
+		CategoryModel.deleteMany(),
+		ProductModel.deleteMany()
+	]);
+
 	// 1. crear usuarios
+	const users = await UserModel.insertMany(seedData.users);
+
 	// 2. crear categorias
+	const categories = await CategoryModel.insertMany(
+		seedData.categories.map((category) => {
+			return {
+				...category,
+				user: users[0]._id
+			};
+		})
+	);
+
 	// 3. crear productos
+	const products = await ProductModel.insertMany(
+		seedData.products.map((product) => {
+			return {
+				...product,
+				user: users[randomBetween0AndX(seedData.users.length - 1)]._id,
+				category:
+					categories[randomBetween0AndX(seedData.categories.length - 1)]._id
+			};
+		})
+	);
+
+	console.log('SEEDED');
 }
