8fd595f7b67e2b1d950d765365b85c1c66d9d0c5
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-07-26 01:39:16 -0500

    316. FileUpload - Ruta y Controlador

---
+++ src/presentation/file-upload/controller.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+import { CustomError } from '../../domain';
+
+export class FileUploadController {
+	constructor() {}
+
+	private handleError = (error: unknown, res: Response) => {
+		if (error instanceof CustomError) {
+			return res.status(error.statusCode).json({ error: error.message });
+		}
+
+		console.log(`${error}`);
+		return res.status(500).json({ error: 'Internal server error' });
+	};
+
+	uploadFile = (req: Request, res: Response) => {
+		res.json('uploadFile');
+	};
+
+	uploadMultipleFiles = (req: Request, res: Response) => {
+		res.json('uploadMultipleFiles');
+	};
+}
---
+++ src/presentation/file-upload/routes.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import { FileUploadController } from './controller';
+
+export class FileUploadRoutes {
+	static get routes(): Router {
+		const router = Router();
+
+		const controller = new FileUploadController();
+
+		router.post('/single/:type', controller.uploadFile);
+
+		router.post('/multiple/:type', controller.uploadMultipleFiles);
+
+		return router;
+	}
+}
