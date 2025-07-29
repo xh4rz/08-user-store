cab8ffb91a7e1f76444e2e5727b0a9e619b5f354
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-07-29 00:55:57 -0500

    319. Cambiar nombre y validar archivo

--- src/presentation/services/file-upload.service.ts
+++ src/presentation/services/file-upload.service.ts
@@ -1,9 +1,11 @@
 import path from 'path';
 import fs from 'fs';
 import type { UploadedFile } from 'express-fileupload';
+import { Uuid } from '../../config';
+import { CustomError } from '../../domain';
 
 export class FileUploadService {
-	constructor() {}
+	constructor(private readonly uuid = Uuid.v4) {}
 
 	private checkFolder(folderPath: string) {
 		if (!fs.existsSync(folderPath)) {
@@ -17,15 +19,26 @@ export class FileUploadService {
 		validExtensions: string[] = ['png', 'jpg', 'jpeg', 'gif']
 	) {
 		try {
-			const fileExtension = file.mimetype.split('/').at(1);
+			const fileExtension = file.mimetype.split('/').at(1) ?? '';
+
+			if (!validExtensions.includes(fileExtension)) {
+				throw CustomError.badRequest(
+					`Invalid extension: ${fileExtension}, valid ones ${validExtensions}`
+				);
+			}
 
 			const destination = path.resolve(__dirname, '../../../', folder);
 
 			this.checkFolder(destination);
 
-			file.mv(destination + `/mi-imagen.${fileExtension}`);
+			const fileName = `${this.uuid()}.${fileExtension}`;
+
+			file.mv(`${destination}/${fileName}`);
+
+			return { fileName };
 		} catch (error) {
 			console.log({ error });
+			throw error;
 		}
 	}
 
