cc00ece855f5bf47ffde5b058e09343b8df093b7
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-07-30 01:57:13 -0500

    323. Middleware para verificar el tipo

--- src/presentation/file-upload/controller.ts
+++ src/presentation/file-upload/controller.ts
@@ -4,7 +4,7 @@ import { FileUploadService } from '../services/file-upload.service';
 import { UploadedFile } from 'express-fileupload';
 
 export class FileUploadController {
-	constructor(private readonly FileUploadService: FileUploadService) {}
+	constructor(private readonly fileUploadService: FileUploadService) {}
 
 	private handleError = (error: unknown, res: Response) => {
 		if (error instanceof CustomError) {
@@ -18,17 +18,10 @@ export class FileUploadController {
 	uploadFile = (req: Request, res: Response) => {
 		const type = req.params.type;
 
-		const validTypes = ['users', 'products', 'categories'];
-
-		if (!validTypes.includes(type)) {
-			return res
-				.status(400)
-				.json({ error: `Invalid type: ${type}, valid ones ${validTypes}` });
-		}
-
 		const file = req.body.files.at(0) as UploadedFile;
 
-		this.FileUploadService.uploadSingle(file, `uploads/${type}`)
+		this.fileUploadService
+			.uploadSingle(file, `uploads/${type}`)
 			.then((uploaded) => res.json(uploaded))
 			.catch((error) => this.handleError(error, res));
 	};
@@ -36,17 +29,10 @@ export class FileUploadController {
 	uploadMultipleFiles = (req: Request, res: Response) => {
 		const type = req.params.type;
 
-		const validTypes = ['users', 'products', 'categories'];
-
-		if (!validTypes.includes(type)) {
-			return res
-				.status(400)
-				.json({ error: `Invalid type: ${type}, valid ones ${validTypes}` });
-		}
-
 		const files = req.body.files as UploadedFile[];
 
-		this.FileUploadService.uploadMultiple(files, `uploads/${type}`)
+		this.fileUploadService
+			.uploadMultiple(files, `uploads/${type}`)
 			.then((uploaded) => res.json(uploaded))
 			.catch((error) => this.handleError(error, res));
 	};
--- src/presentation/file-upload/routes.ts
+++ src/presentation/file-upload/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { FileUploadController } from './controller';
 import { FileUploadService } from '../services/file-upload.service';
 import { FileUploadMiddleware } from '../middlewares/file-upload.middleware';
+import { TypeMiddleware } from '../middlewares/type.middleware';
 
 export class FileUploadRoutes {
 	static get routes(): Router {
@@ -10,6 +11,8 @@ export class FileUploadRoutes {
 		const controller = new FileUploadController(new FileUploadService());
 
 		router.use(FileUploadMiddleware.containFiles);
+
+		router.use(TypeMiddleware.validTypes(['users', 'products', 'categories']));
 
 		router.post('/single/:type', controller.uploadFile);
 
