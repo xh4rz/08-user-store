0c10c224035851bff852c4bff6f051dc105ab23f
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-07-30 01:24:49 -0500

    322. Carga múltiple de archivos

--- src/presentation/file-upload/controller.ts
+++ src/presentation/file-upload/controller.ts
@@ -34,6 +34,20 @@ export class FileUploadController {
 	};
 
 	uploadMultipleFiles = (req: Request, res: Response) => {
-		res.json('uploadMultipleFiles');
+		const type = req.params.type;
+
+		const validTypes = ['users', 'products', 'categories'];
+
+		if (!validTypes.includes(type)) {
+			return res
+				.status(400)
+				.json({ error: `Invalid type: ${type}, valid ones ${validTypes}` });
+		}
+
+		const files = req.body.files as UploadedFile[];
+
+		this.FileUploadService.uploadMultiple(files, `uploads/${type}`)
+			.then((uploaded) => res.json(uploaded))
+			.catch((error) => this.handleError(error, res));
 	};
 }
